8c85e5a6600665689e54acca621e4faba10ca25f
Shiv-hiremath01 <shivbh2001@gmail.com>
2025-05-18 13:36:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -121,7 +121,8 @@ require('dotenv').config();
 const app = express();
 
 // Use CORS middleware
-app.use(cors());
+app.use(cors({ origin: '*', credentials: true }));
+
 
 // Serve static files from the React app's build folder
 app.use(express.static(path.join(__dirname, '../frontend/build')));
@@ -223,4 +224,7 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running at: http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at: http://0.0.0.0:${PORT}`);
+});
+
